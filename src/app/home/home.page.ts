2ba6692ae6017988042ca2e19c30e77baa2e9f2e
Alejandro Coronado <coronadoa@globalhitss.com>
2019-05-20 11:46:28 -0700

    map works- new baseline

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps/ngx'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  map: GoogleMap;
+
+  constructor(private platform: Platform){
+
+  }
+
+  async ngOnInit(){
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap(){
+    this.map = GoogleMaps.create('map_canvas');
+    
+  }
 
 }
